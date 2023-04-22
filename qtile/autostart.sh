c76455b7b94da8a030194ae00f5b21de5676d05c
Patricio Briceño <120836182+patricio-alexander@users.noreply.github.com>
2023-04-22 12:34:44 -0500

    update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,7 +3,13 @@
 #wallpaper
 feh --bg-fill /home/patricio/Images/wallpapers/wallp2.png
 
+#picom
+ 
+#picom --experimental-backends 
+picom --config $HOME/.config/qtile/picom/picom.conf &
 
+#wifi icon
+nm-applet &
 
 #start sxhkd to replace Qtile native key-bindings
 #run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
